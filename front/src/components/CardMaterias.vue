8eee43ea336e7ec1c66d6f79ba9c2cc302f52d0b
Porfirio-Prodigy <prodigy.porfirio@gmail.com>
2022-08-06 20:30:48 -0300

    refactor: restruturação do código

---
+++ front/src/components/CardMaterias.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Card Materias</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CardMaterias'
+})
+</script>
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page class="flex flex-center">
+    <CardMaterias></CardMaterias>
     <h6>Welcome To Artemis</h6>
   </q-page>
 </template>
@@ -7,21 +8,21 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import { api } from 'boot/axios.js'
+import CardMaterias from '../components/CardMaterias.vue'
 
 export default defineComponent({
   name: 'IndexPage',
-
   setup () {
     return {
       first: ref('')
     }
   },
-
   created () {
     api.get('/')
       .then((response) => {
         console.log(JSON.stringify(response.data))
       })
-  }
+  },
+  components: { CardMaterias }
 })
 </script>
